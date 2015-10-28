f1bd13cebf3c4144021b41c5ff2891f7ac4d548d
Shinmera <shinmera@tymoon.eu>
2015-10-28 18:48:54 +0100

    Added REPAINTABLE low-level construct

---
+++ repaintable.lisp
@@ -0,0 +1,16 @@
+#|
+ This file is a part of Qtools-UI
+ (c) 2015 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:org.shirakumo.qtools.ui)
+(in-readtable :qtools)
+
+(define-widget repaintable (QWidget)
+  ())
+
+(define-signal (repaintable repaint) ())
+
+(define-initializer (repaintable setup)
+  (connect! repaintable (repaint) repaintable (update)))
