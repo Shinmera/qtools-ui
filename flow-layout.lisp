3077217d827fc9ee5b38023a8dda17793dd70fbb
Shinmera <shinmera@tymoon.eu>
2015-11-14 16:04:48 +0100

    Flow-layout.

---
+++ flow-layout.lisp
@@ -0,0 +1,20 @@
+#|
+ This file is a part of Qtools-UI
+ (c) 2015 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:org.shirakumo.qtools.ui)
+(in-readtable :qtools)
+
+(define-widget flow-layout (QWidget container)
+  ())
+
+(defmethod update ((flow-layout flow-layout))
+  (loop for maxheight = 0 then (max maxheight (q+:height widget))
+        for x = 0 then (+ x (q+:width widget))
+        for widget in (widgets flow-layout)
+        for y = 0 then (if (< (q+:width flow-layout) (+ x (q+:width widget)))
+                           (+ y (prog1 maxheight (setf maxheight 0) (setf x 0)))
+                           y)
+        do (setf (q+:geometry widget) (values x y (q+:width widget) (q+:height widget)))))
---
+++ qtools-ui-flow-layout.asd
@@ -0,0 +1,17 @@
+#|
+ This file is a part of Qtools-UI
+ (c) 2015 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:cl-user)
+(asdf:defsystem qtools-ui-flow-layout
+  :license "Artistic"
+  :author "Nicolas Hafner <shinmera@tymoon.eu>"
+  :maintainer "Nicolas Hafner <shinmera@tymoon.eu>"
+  :description "A flow-layout that lets widgets flow to the next line."
+  :homepage "https://github.com/Shinmera/qtools-ui"
+  :serial T
+  :components ((:file "flow-layout"))
+  :depends-on (:qtools-ui-base
+               :qtools-ui-layout))
