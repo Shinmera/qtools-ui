5da02e9f8c43cd3c9726f4722c203b796703943c
Shinmera <shinmera@tymoon.eu>
2019-01-16 16:07:28 +0100

    Change byte-array functions to be hot-patching.
    This is because the method ID that fast-call caches can apparently
    change on image reload. I don't know yet why or how that happens,
    but once it does and an appropriate fix has been made (somewhere)
    this commit can be reverted again.

--- bytearray.lisp
+++ bytearray.lisp
@@ -10,13 +10,21 @@
 ;;; We (ab)use fast-calls to pass around pointers that the usual
 ;;; CommonQt would mistakenly try to translate to strings.
 
-(defun from-byte-array (bytes &optional (finalize T))
+(defmacro define-hot-patching (name args &body body)
+  (let ((first-args (gensym "ARGUMENTS")))
+    `(defun ,name (&rest ,first-args)
+       (apply
+        (compile ',name '(lambda ,args
+                          ,@body))
+        ,first-args))))
+
+(define-hot-patching from-byte-array (bytes &optional (finalize T))
   (prog1 (cffi:foreign-array-to-lisp
           (fast-call (data qbytearray "void**") bytes)
           `(:array :uchar ,(fast-call (length qbytearray "int") bytes)))
     (when finalize (optimized-delete bytes))))
 
-(defun to-byte-array (data)
+(define-hot-patching to-byte-array (data)
   (cffi:with-pointer-to-vector-data (pointer data)
     (let ((bytes (fast-static-call (qbytearray-from-raw-data "QByteArray" "const char*" "int")
                                    pointer (length data))))
